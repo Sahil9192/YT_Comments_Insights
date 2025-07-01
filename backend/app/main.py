28cad03cae4d0596d6f8ea69f473b0a63a4fdd67
Sahil9192 <sahilypawar9192@gmail.com>
2025-07-01 11:19:25 +0530

    Changes added in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.youtube_api import fetch_comments
 from app.sentiment_analysis import analyze_sentiment
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
